18e9ff72b3a8583704129ed02c182a7bcd583ea1
Edvan Ronchi <edvan_ronchi@hotmail.com>
2024-05-18 18:19:11 -0300

    #:feat: Configurado application e suas dependencias.

--- src/main/java/dev/edvanronchi/springbootautomatedtest/SpringBootAutomatedTestApplication.java
+++ src/main/java/dev/edvanronchi/springbootautomatedtest/SpringBootAutomatedTestApplication.java
@@ -3,7 +3,7 @@ package dev.edvanronchi.springbootautomatedtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "dev.edvanronchi.springbootautomatedtest")
 public class SpringBootAutomatedTestApplication {
 
     public static void main(String[] args) {
